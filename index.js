abea71dd6de0f0be1a6239d191db834a6ad41d2e
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-05-23 11:37:32 -0400

    Restructured gamePiece class and broke up moveCharacter function and made it a property of the gamePiece class.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 //Heather C. Starkie
 //Platformer.js
 
+
 //global variables//
 var myGamePiece;
-var speed = 3;
+var speedLimit = 3;
 var friction = 0.8;
 var keys = [];
 ////////////////////
@@ -28,7 +29,6 @@ var gameArea = {
 
 }
 
-
 function gamePiece(width,height,color,x,y){
 	this.width = width;
 	this.height = height;
@@ -40,6 +40,36 @@ function gamePiece(width,height,color,x,y){
 	this.speedY = 0;
 	this.jumping = false;
 
+	this.bounds = {
+		ground: gameArea.canvas.height - this.height,
+		leftWall: 0,
+		rightWall: gameArea.canvas.width
+	}
+
+	this.btnToMoveLeft = 65; //Key: A
+	this.btnToMoveRight = 68; //Key: D
+	this.btnToJump = 32; //Key: SPACE
+	
+	/////METHODS////
+	this.checkInput = function(){
+		//button to move left
+		if(keys[this.btnToMoveLeft]){
+			if(this.speedX > -speedLimit){
+				this.speedX -= 0.1;
+			}
+		}
+		//button to move right
+		if(keys[this.btnToMoveRight]){
+			if(this.speedX < speedLimit){
+				this.speedX += 0.1;
+			}
+		}
+		//button to jump
+		if(keys[this.btnToJump]){
+			this.jumping = true;
+		}
+	}
+	
 	this.update = function(){
 		ctx = gameArea.context;
 		ctx.fillStyle = color;
@@ -53,53 +83,28 @@ function gamePiece(width,height,color,x,y){
 	}
 	//CD stands for collision detection
 	this.collisionDetection = function(){
- 		//variables holding positions of boundaries
-		var ground = gameArea.canvas.height - this.height;
-		/*var rightWall = gameArea.canvas.width - this.width;*/
-		//check ground
-		if(this.y > ground){
-			this.y = ground;
+		//check ground collision
+		if(this.y > this.bounds.ground){
+			this.y = this.bounds.ground;
 		}
-		//check left wall
-		if(this.x < 0){
-			this.x = 0;
+		//check left wall collision 
+		if(this.x < this.bounds.leftWall){
+			this.x = this.bounds.leftWall;
 		}
-
+		//check right wall collision
+		if(this.x > this.bounds.rightWall){
+			this.x = this.bounds.rightWall;
+		} 
 	}
 }
+
 
 function updateGame(){
 	gameArea.clear();
 	myGamePiece.newPos ();
 	myGamePiece.update();
+	myGamePiece.checkInput();
 
-}
-
-
-
-function moveCharacter(){
-	var ground = gameArea.canvas.height - this.myGamePiece.height;
-	/*console.log("X:" + this.myGamePiece.x);
-	console.log("Y:" + this.myGamePiece.y);*/
-	//'spacebar'
-	if(keys[32]){
-		if(this.myGamePiece.jumping === false && this.myGamePiece.y === ground){
-			this.myGamePiece.jumping = true;
-			this.myGamePiece.speedY -= speed*2;
-		}
-	}
-	//'A'
-	if(keys[65]){
-		if(this.myGamePiece.speedX > -speed){
-			this.myGamePiece.speedX -= 1;
-		}
-	}
-	//'D'
-	if(keys[68]){
-		if(this.myGamePiece.speedX < speed){
-			this.myGamePiece.speedX += 1;
-		}
-	}
 }
 
 function stopCharacter(e){
